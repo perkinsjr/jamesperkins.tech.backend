24346a32d8c58d863e612e9d02b78e3ba9ef3349
James Perkins <jamesperkins@hey.com>
2021-02-12 15:52:37 -0500

    adding cloudinary

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8082),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e85373a70f1f89cff5737c0fb0895eb9'),
+      secret: env("ADMIN_JWT_SECRET", "e85373a70f1f89cff5737c0fb0895eb9"),
     },
   },
 });
